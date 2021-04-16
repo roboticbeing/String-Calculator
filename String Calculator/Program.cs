5346025acface0f91ece7fe01eb2278251e60572
roboticbeing <jessicakrishtul@gmail.com>
2021-04-15 20:45:33 -0400

    Support a custom delimeter

--- String Calculator/Program.cs
+++ String Calculator/Program.cs
@@ -12,15 +12,21 @@ namespace Program
             //e.Write tests to prove your input validates.
 
             //d.Example input: “1,2,5” -expected result: “8”.
-            Console.WriteLine(sc.Add("1,2,5"));
-            //Example input: “” -expected result: “0”.
-            Console.WriteLine(sc.Add("0"));
-            //a. Example: “1\n,2,3” -Result: “6”
-            Console.WriteLine(sc.Add("1\n,2,3"));
-            //b.Example 2: “1,\n2,4” -Result: “7”
-            Console.WriteLine(sc.Add("1\n,2,4"));
+            //Console.WriteLine(sc.Add("1,2,5"));
+            ////Example input: “” -expected result: “0”.
+            //Console.WriteLine(sc.Add("0"));
+            ////a. Example: “1\n,2,3” -Result: “6”
+            //Console.WriteLine(sc.Add("1\n,2,3"));
+            ////b.Example 2: “1,\n2,4” -Result: “7”
+            //Console.WriteLine(sc.Add("1\n,2,4"));
 
-
+            //PART 2
+            //c.Example: “//;\n1;3;4” - Result: 8
+            Console.WriteLine(sc.Add("//;\n1;3;4"));
+            //i. “//$\n1$2$3” - Result: 6
+            Console.WriteLine(sc.Add("//$\n1$2$3"));
+            //ii. “//@\n2@3@8” - Result: 13
+            Console.WriteLine(sc.Add("//@\n2@3@8"));
         }
     }
 }
--- String Calculator/StringCalculator.cs
+++ String Calculator/StringCalculator.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 
+
+
 namespace String_Calculator
 {
     class StringCalculator
     {
         //1.Create a simple String calculator with a method: int Add(string numbers)
         //2.Change the Add method to handle new lines in the input format - it already handles new lines.
+        //3. 3.Support a custom delimiter
         public int Add(string numbers)
         {
             //a.The numbers in the string are separated by a comma.
-            string[] nums = numbers.Split(',');
+            //string[] nums = numbers.Split(',');
+
+            //a. The beginning of your string will now contain a small control code that lets you set a custom delimiter.
+            //b. Format: “//[delimiter]\n[delimiter separated numbers]”
+            string delimeter = numbers.Substring(numbers.IndexOf("//") + 2, 1);
+            string[] nums = numbers.Split(delimeter);
             int sum = 0;
 
             //b.Empty strings should return 0.
@@ -20,7 +28,11 @@ namespace String_Calculator
 
             foreach (var num in nums)
             {
-                sum += Int32.Parse(num);
+                if(!(num == "//"))
+                {
+                    sum += Int32.Parse(num);
+                }
+                
             }
 
             //c.The return type is an integer.
