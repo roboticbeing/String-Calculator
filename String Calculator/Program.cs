d50d468f5e1c2cf089c1ea5795d74d053a60e330
roboticbeing <jessicakrishtul@gmail.com>
2021-04-16 10:21:34 -0400

    Create custom delimeter tests

--- String Calculator/Program.cs
+++ String Calculator/Program.cs
@@ -9,25 +9,22 @@ namespace Program
         {
             Tests test = new Tests();
 
-            //Example input: “1,2,5” -expected result: “8”.
+            //input: “1,2,5” -expected result: “8”.
             test.CommaSplitTest("1,2,5");
-            //Example input: “2,2,2” -expected result: “6”.
+            //input: “2,2,2” -expected result: “6”.
             test.CommaSplitTest("2,2,2");
-            ////Example input: “” -expected result: “0”.
+            ////input: “” -expected result: “0”.
             test.EmptyStringTest("");
-
-            ////a. Example: “1\n,2,3” -Result: “6”
-            //Console.WriteLine(sc.Add("1\n,2,3"));
-            ////b.Example 2: “1,\n2,4” -Result: “7”
-            //Console.WriteLine(sc.Add("1\n,2,4"));
-
-            //PART 3
-            //c.Example: “//;\n1;3;4” - Result: 8
-            //Console.WriteLine(sc.Add("//;\n1;3;4"));
-            ////i. “//$\n1$2$3” - Result: 6
-            //Console.WriteLine(sc.Add("//$\n1$2$3"));
-            ////ii. “//@\n2@3@8” - Result: 13
-            //Console.WriteLine(sc.Add("//@\n2@3@8"));
+            //input: “1\n,2,3” -Result: “6”
+            test.NewLineTest("1\n,2,3");
+            //input: “1,\n2,4” -Result: “7”
+            test.NewLineTest("1,\n2,4");
+            //input: “//;\n1;3;4” - Result: 8
+            test.CustomDelimeterTest("//;\n1;3;4");
+            ////input: “//$\n1$2$3” - Result: 6
+            test.CustomDelimeterTest("//$\n1$2$3");
+            ////input: “//@\n2@3@8” - Result: 13
+            test.CustomDelimeterTest("//@\n2@3@8");
 
             //PART 4
             // Console.WriteLine(sc.Add("//,-1,-2,3,4"));
--- String Calculator/Tests.cs
+++ String Calculator/Tests.cs
@@ -7,6 +7,61 @@ namespace String_Calculator
       //Write tests to prove your input validates.
     class Tests : StringCalculator
     {
+        public bool CustomDelimeterTest(string input)
+        {
+            int expected_result = 0;
+            switch (input)
+            {
+                case "//;\n1;3;4":
+                    expected_result = 8;
+                    break;
+                case "//$\n1$2$3":
+                    expected_result = 6;
+                    break;
+                case "//@\n2@3@8":
+                    expected_result = 13;
+                    break;
+            }
+
+            if (Add(input) != expected_result)
+            {
+                Console.WriteLine("Custom Delimeter Test failed: " + Add(input) + " did not match " + expected_result);
+                return false;
+            }
+
+            else
+            {
+                Console.WriteLine("Custome Delimeter Test passed: " + Add(input));
+                return true;
+            }
+        }
+
+        public bool NewLineTest(string input)
+        {
+            int expected_result = 0;
+            switch (input)
+            {
+                case "1\n,2,3":
+                    expected_result = 6;
+                    break;
+                case "1,\n2,4":
+                    expected_result = 7;
+                    break;
+            }
+
+            if (Add(input) != expected_result)
+            {
+                Console.WriteLine("New Line Test failed: " + Add(input) + " did not match " + expected_result);
+                return false;
+            }
+
+            else
+            {
+                Console.WriteLine("New Line Test passed: " + Add(input));
+                return true;
+            }
+        }
+
         public bool EmptyStringTest(string input)
         {
             int expected_result = 0;
