2f53746451ae96daa8f9123e6d6071307fa4e0cd
roboticbeing <jessicakrishtul@gmail.com>
2021-04-15 20:01:17 -0400

    Accept new lines in input format

--- String Calculator/Program.cs
+++ String Calculator/Program.cs
@@ -9,8 +9,18 @@ namespace Program
         {
             StringCalculator sc = new StringCalculator();
 
+            //e.Write tests to prove your input validates.
+
             //d.Example input: “1,2,5” -expected result: “8”.
             Console.WriteLine(sc.Add("1,2,5"));
+            //Example input: “” -expected result: “0”.
+            Console.WriteLine(sc.Add("0"));
+            //a. Example: “1\n,2,3” -Result: “6”
+            Console.WriteLine(sc.Add("1\n,2,3"));
+            //b.Example 2: “1,\n2,4” -Result: “7”
+            Console.WriteLine(sc.Add("1\n,2,4"));
+
+
         }
     }
 }
--- String Calculator/StringCalculator.cs
+++ String Calculator/StringCalculator.cs
@@ -2,18 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
-//Write tests
-
-
-
-//e.Write tests to prove your input validates.
-
-
 namespace String_Calculator
 {
     class StringCalculator
     {
         //1.Create a simple String calculator with a method: int Add(string numbers)
+        //2.Change the Add method to handle new lines in the input format - it already handles new lines.
         public int Add(string numbers)
         {
             //a.The numbers in the string are separated by a comma.
